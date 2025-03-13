988c4c79031c77692d7bdf9b4c63fd0c2699f187
sakana-max <tatetomo08@gmail.com>
2025-03-13 14:59:32 +0900

    Improvements and added features
    Added confirmation of file name and whether to overwrite

--- Image_information_embedder.py
+++ Image_information_embedder.py
@@ -1,6 +1,25 @@
 from m_src import decoder_m
 from m_src import encoder_m
+import os
 
+
+def Enter(text="",mode=""):
+    while True:
+        inputtext= input(text)
+        if(mode == "A"):
+            if(os.path.isfile(inputtext)):
+                break
+            else:
+                print("caveat[2]!There is no file path to specify. Please re-enter")
+        if(mode == "B"):
+            if(os.path.isfile(inputtext)):
+                print("caveat[3]!A file with the same name already exists. If you leave it as is, it will be overwritten when decoding and outputting. ")
+                Input = input("Are you sure? Yes or No:1 or other input:")
+                if Input == "1":
+                    break
+            else:
+                break
+    return inputtext
 #モードを聞く
 #Listen to the mode
 mode = input("Enter mode:encode or decode:0 or 1:")
@@ -11,10 +30,17 @@ if(mode == "0"):
   #必要情報を取得(格納するファイルのパス、デコードした際のファイル名、書き込む画像パス、出力する画像パス)
   #Get required information (path of file to store, file name when decoded, image path to write, image path to output)
   while True:
-     S= input ("Enter the file path to store:")
-     O= input ("Enter the file path to output:")
-     A= input ("Enter the path of the image to be written:")
-     B= input ("Enter the image file path to store:")
+     S = Enter("Enter the file path to store:","A")
+     
+     O = Enter("Enter the file path to output:","B")
+     
+     A = Enter("Enter the path of the image to be written:","A")
+     
+     B = Enter("Enter the file path to output:","B")
+     print(S)
+     print(O)
+     print(A)
+     print(B)
      confirmation = input("Is the above content correct?:No or Yes:1 or Other input:")
      if confirmation != "1":
          break
@@ -46,7 +72,8 @@ elif(mode == "1"):
      #読み込む画像のパスを取得
      #Get the path of the image to be loaded
      while True:
-        A= input ("Enter the path of the image you want to load:")
+        A=Enter("Enter the path of the image you want to load:","A")
+        print(A)
         confirmation = input("Is the above content correct?:No or Yes:1 or Other input:")
         if confirmation != "1":
            break
@@ -55,6 +82,15 @@ elif(mode == "1"):
      #Read binary information and filename from image
      decodet,filepas = decoder_m.rood_image_bytes(A)
 
+     while True:
+         if(os.path.isfile(filepas)):
+                print(filepas)
+                print("A file with the same name already exists. If you leave it as is, it will be overwritten when decoding and outputting. ")
+                Input = input("Are you sure? Yes or No:1 or other input:")
+                if Input == "1":
+                    break
+         else:
+                break
      #ファイル名を出力
      #Output file name
      print("Loading successful Output:" + filepas)
